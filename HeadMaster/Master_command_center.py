f782661e8b68e75fb2f0d5979dbe7406f2917a3f
Meik -- <meikklein123@gmail.com>
2024-12-06 20:38:56 +0100

    Handshake

--- HeadMaster/Master_command_center.py
+++ HeadMaster/Master_command_center.py
@@ -2,7 +2,7 @@ import socket
 import time
 
 # Socket-Setup
-HOST = '0.0.0.0'  # Alle Schnittstellen
+HOST = '0.0.0.0'  # Hört auf alle Schnittstellen
 PORT = 12345
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
@@ -13,21 +13,27 @@ conn, addr = sock.accept()
 print(f"Verbunden mit {addr}")
 
 try:
+    # Handshake: Warte auf Nachricht vom Raspberry Pi
+    message = conn.recv(1024).decode('utf-8').strip()
+    print("Handshake erhalten:", message)
+    if message == "HELLO_PC":
+        conn.send("HELLO_PI\n".encode('utf-8'))
+        print("Antwort gesendet: HELLO_PI")
+    else:
+        print("Unerwartete Handshake-Nachricht:", message)
+
+    # Hauptschleife (Dummy-Daten empfangen und antworten)
     while True:
-        # Encoder-Daten vom Raspberry Pi empfangen
-        data = conn.recv(1024).decode('utf-8')
-        if data:
-            left_ticks, right_ticks = map(int, data.split(","))
-            print(f"Encoder: {left_ticks}, {right_ticks}")
+        data = conn.recv(1024).decode('utf-8').strip()
+        print("Daten empfangen:", data)
         
-        # Steuerbefehle senden (z. B. Geschwindigkeit)
-        left_speed = 50  # Beispielwert
-        right_speed = 50  # Beispielwert
-        conn.send(f"{left_speed},{right_speed}\n".encode('utf-8'))
-        time.sleep(0.1)
+        # Antwort senden
+        response = "RECEIVED:" + data + "\n"
+        conn.send(response.encode('utf-8'))
+        print("Antwort gesendet:", response.strip())
 
 except KeyboardInterrupt:
-    print("Beendet")
+    print("Beendet durch Benutzer")
 finally:
     conn.close()
     sock.close()
--- raspberry_pi/Pi_hardware_interface.py
+++ raspberry_pi/Pi_hardware_interface.py
@@ -1,50 +1,45 @@
 import RPi.GPIO as GPIO
-import time
 import socket
+import time
 
-# GPIO-Setup
-AIN1 = 17
-AIN2 = 18
-PWM_FREQ = 50
-
+# GPIO-Setup (falls benötigt)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(AIN1, GPIO.OUT)
-GPIO.setup(AIN2, GPIO.OUT)
-
-pwm1 = GPIO.PWM(AIN1, PWM_FREQ)
-pwm2 = GPIO.PWM(AIN2, PWM_FREQ)
-pwm1.start(0)
-pwm2.start(0)
 
 # Socket-Setup
-HOST = '192.168.178.20'  # IP-Adresse des PCs
+HOST = '192.168.0.104'  # IP-Adresse des PCs
 PORT = 12345
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("Versuche Verbindung mit dem PC...")
 sock.connect((HOST, PORT))
+print("Verbindung hergestellt!")
 
 try:
+    # Handshake: Sende Testnachricht an PC
+    sock.send("HELLO_PC\n".encode('utf-8'))
+    print("Handshake gesendet: HELLO_PC")
+
+    # Warte auf Antwort vom PC
+    response = sock.recv(1024).decode('utf-8').strip()
+    if response == "HELLO_PI":
+        print("Handshake erfolgreich: Antwort vom PC erhalten ->", response)
+    else:
+        print("Unerwartete Antwort vom PC:", response)
+
+    # Hauptschleife (Dummy-Daten senden)
     while True:
-        # Daten vom PC empfangen
-        data = sock.recv(1024).decode('utf-8')
-        if data:
-            left_speed, right_speed = map(int, data.split(","))
-            
-            # PWM-Signale setzen
-            pwm1.ChangeDutyCycle(left_speed)
-            pwm2.ChangeDutyCycle(right_speed)
+        data_to_send = "DATA_FROM_PI,42,84\n"
+        sock.send(data_to_send.encode('utf-8'))
+        print("Gesendet:", data_to_send.strip())
         
-        # Encoder-Werte simulieren (z. B. Zufallswerte oder GPIO-Eingaben)
-        left_ticks = 10  # Hier echte Encoder-Daten verwenden
-        right_ticks = 12  # Hier echte Encoder-Daten verwenden
-        
-        # Daten an PC senden
-        sock.send(f"{left_ticks},{right_ticks}\n".encode('utf-8'))
-        time.sleep(0.1)
+        # Warte auf Antwort (zum Test)
+        response = sock.recv(1024).decode('utf-8').strip()
+        print("Antwort vom PC:", response)
+
+        time.sleep(1)
 
 except KeyboardInterrupt:
-    print("Beendet")
+    print("Beendet durch Benutzer")
 finally:
-    pwm1.stop()
-    pwm2.stop()
-    GPIO.cleanup()
     sock.close()
+    GPIO.cleanup()
