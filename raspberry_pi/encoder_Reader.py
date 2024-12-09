9482eff30f7709f14944c1867f61b8e397e85007
Meik -- <meikklein123@gmail.com>
2024-12-09 19:52:16 +0100

    Maybe working now?

--- raspberry_pi/encoder_Reader.py
+++ raspberry_pi/encoder_Reader.py
@@ -9,6 +9,9 @@ class EncoderReader:
         self.E2B = E2B_Pin #Motor 2
         self.spin_count1 = 0 #Motor 1
         self.spin_count2 = 0 #Motor 2
+        self.last_time1 = 0  # Letztes Ereignis für Encoder 1
+        self.last_time2 = 0  # Letztes Ereignis für Encoder 2
+        self.debounce_time = 0.002  # Entprellzeit in Sekunden
         self.setup_gpio()
 
     def setup_gpio(self):
@@ -17,9 +20,9 @@ class EncoderReader:
         GPIO.setup(self.E2A,GPIO.IN,pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.E2B,GPIO.IN,pull_up_down=GPIO.PUD_UP)
         GPIO.add_event_detect(self.E1A, GPIO.BOTH, callback=self.myCallBack1)
-        GPIO.add_event_detect(self.E1B, GPIO.BOTH, callback=self.myCallBack1)
+        #GPIO.add_event_detect(self.E1B, GPIO.BOTH, callback=self.myCallBack1)
         GPIO.add_event_detect(self.E2A, GPIO.BOTH, callback=self.myCallBack2)
-        GPIO.add_event_detect(self.E2B, GPIO.BOTH, callback=self.myCallBack2)
+        #GPIO.add_event_detect(self.E2B, GPIO.BOTH, callback=self.myCallBack2)
     def GetValues(self):
         return self.spin_count1,self.spin_count2
               
@@ -28,6 +31,10 @@ class EncoderReader:
         Callback für Encoder 1 (Motor 1).
         Prüft Signal A und B, um die Drehrichtung zu bestimmen.
         """
+        current_time = time.time()
+        if current_time - self.last_time1 < self.debounce_time:
+            return  # Ignoriere dieses Ereignis (Entprellung)
+        self.last_time1 = current_time
         A = GPIO.input(self.E1A)  # Zustand von Signal A
         B = GPIO.input(self.E1B)  # Zustand von Signal B
 
@@ -42,6 +49,10 @@ class EncoderReader:
         Callback für Encoder 2 (Motor 2).
         Funktioniert analog zu myCallBack1.
         """
+        current_time = time.time()
+        if current_time - self.last_time2 < self.debounce_time:
+            return  # Ignoriere dieses Ereignis (Entprellung)
+        self.last_time2 = current_time
         A = GPIO.input(self.E2A)  # Zustand von Signal A
         B = GPIO.input(self.E2B)  # Zustand von Signal B
 
